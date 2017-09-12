2b7bc41269763f17a18876c876340176d21efb81
mcallegari10 <martin.callegari@wolox.com.ar>
2017-09-12 12:14:13 -0300

    changed the react router to its own component

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Router, Route } from 'react-router-dom';
+
+import history from '../history';
 import Dashboard from './components/Dashboard';
+import BookDetail from './components/BookDetail';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
     return (
-      <Dashboard />
+      <Router history={history}>
+        <div>
+          <Route exact path='/' component={ Dashboard } />
+          <Route exact path='/login' component={ Login } />
+          <Route exact path='/book/:bookId' component={ BookDetail } />
+        </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router-dom';
-import history from './history';
 
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
-import Dashboard from './App/components/Dashboard';
-import BookDetail from './App/components/BookDetail';
-import Login from './App/components/Login';
+
 import './index.css';
 
 ReactDOM.render(
-  <Router history={history}>
-    <div>
-      <Route exact path='/' component={ App } />
-      <Route exact path='/login' component={ Login } />
-      <Route exact path='/dashboard' component={ Dashboard } />
-      <Route exact path='/book/:bookId' component={ BookDetail } />
-    </div>
-  </Router>,
+  <App />,
   document.getElementById('root')
 );
 registerServiceWorker();
